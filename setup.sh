038d0370e2e1adf4f9e5851d7ce186e6dc1dbc57
south-str <k_k_kanon_milenium@hotmail.co.jp>
2017-04-03 12:47:29 +0900

    if文の条件を修正した

--- setup.sh
+++ setup.sh
@@ -3,12 +3,20 @@
 CURRENT_DIRECTORY=$(pwd)
 
 # ファイルを格納するためのディレクトリを作成する
-if [ -f =/.vim ] ; then
+echo "make directories"
+if [ -d ~/.vim ] ; then
+  echo "~/.vim directory is exists"
+else
   mkdir ~/.vim
 fi
 # 設定ファイルのシンボリックリンクを$HOME配下に配置する
 # 本当はループで回す方がいいけど、まだ数が少ないので直書きで
+echo "create links"
 ln -s ${CURRENT_DIRECTORY}/.bash_profile ~/.bash_profile
 ln -s ${CURRENT_DIRECTORY}/.bashrc ~/.bashrc
 ln -s ${CURRENT_DIRECTORY}/.vimrc ~/.vimrc
-ln -s ${CURRENT_DIRECTORY}/.vim/plugin.vim ~/.vim/plugin.vim
+if [ -d ~/.vim ] ; then
+  ln -s ${CURRENT_DIRECTORY}/.vim/plugin.vim ~/.vim/plugin.vim
+else
+  echo "~/.vim directory is not exists"
+fi
